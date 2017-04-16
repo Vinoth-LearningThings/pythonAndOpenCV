7f188410cf4d63b6787676ad951c1da21bd29f40
MyPair <vinothcodesnow@gmail.com>
2017-04-17 00:18:03 +0530

    resize output window

--- FromOfflineVideo.py
+++ FromOfflineVideo.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
-cam = cv2.VideoCapture('vlc.mp4');
+cam = cv2.VideoCapture('airport_footage.mp4');
 
 print cam.isOpened();
 
@@ -12,7 +12,10 @@ while(cam.isOpened()):
     faces = faceDetect.detectMultiScale(gray, 1.3, 5);
     for(x,y,w,h) in faces:
         cv2.rectangle(img, (x,y),(x+w,y+h), (0,255,0), 2)
+    cv2.namedWindow('faceDetector',cv2.WINDOW_NORMAL);
+    cv2.resizeWindow('faceDetector', 600,300);
     cv2.imshow("faceDetector", img);
+    
     if(cv2.waitKey(1) == ord('q')):
         break;
 cam.release()
